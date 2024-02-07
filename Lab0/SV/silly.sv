3feec284b4c83ca34c61907c32594eee70b52d97
EthanielCook <158609492+EthanielCook@users.noreply.github.com>
2024-02-07 12:26:44 -0600

    Add files via upload

---
+++ Lab0/SV/silly.sv
@@ -0,0 +1,6 @@
+module silly (input  logic a, b, c, output logic cout, sum);
+   
+  assign cout = a & b & ~c  | ~a & b & c | a & ~b & c  | a & b & c;
+  assign sum = a & ~b & ~c | ~a & b & ~c | ~a & ~b & c | a & b & c;
+  
+endmodule
---
+++ Lab0/SV/silly_tb.sv
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+module tb ();
+
+   logic    a;
+   logic 	b;
+   logic 	c;
+   logic 	sum;
+   logic 	cout;
+   logic    clk; 
+   
+  // instantiate device under test
+   silly dut (a, b, c, sum, cout);
+
+   // 2 ns clock
+   initial 
+     begin	
+	clk = 1'b1;
+	forever #10 clk = ~clk;
+     end
+
+
+   initial
+begin
+    #0   a = 1'b0;    
+    #0   b = 1'b0;    
+    #0   c = 1'b0;
+
+    #20  a = 1'b0;    
+    #0   b = 1'b0;    
+    #0   c = 1'b1;
+
+    #20  a = 1'b0;    
+    #0   b = 1'b1;    
+    #0   c = 1'b0;    
+
+    #20  a = 1'b0;    
+    #0   b = 1'b1;    
+    #0   c = 1'b1;    
+
+    #20  a = 1'b1;    
+    #0   b = 1'b0;    
+    #0   c = 1'b0;    
+
+    #20  a = 1'b1;    
+    #0   b = 1'b0;    
+    #0   c = 1'b1;    
+
+    #20  a = 1'b1;    
+    #0   b = 1'b1;    
+    #0   c = 1'b0;    
+
+    #20  a = 1'b1;    
+    #0   b = 1'b1;    
+    #0   c = 1'b1;    
+end
+
+   
+endmodule
